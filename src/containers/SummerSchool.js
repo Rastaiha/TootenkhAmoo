da42cd5197aa4332775bb85735a392b679b3101a
amoohashem <amoo.hashem.mehraban@gmail.com>
2021-08-20 06:13:12 +0430

    add starts for more beauty demo!

--- src/containers/SummerSchool.js
+++ src/containers/SummerSchool.js
@@ -50,7 +50,34 @@ const Index = () => {
     <Stage width={window.innerWidth} height={window.innerHeight}>
       <Layer >
         <URLImage width={window.innerWidth} height={window.innerHeight} src={process.env.PUBLIC_URL + '/backgrounds/space.jpg'} />
-        <LionImage />
+        {/* <LionImage /> */}
+      </Layer>
+      <Layer>
+        {/* <Text text="Try to drag a star" /> */}
+        {stars.map((star) => (
+          <Star
+            key={star.id}
+            id={star.id}
+            x={star.x}
+            y={star.y}
+            numPoints={5}
+            innerRadius={20}
+            outerRadius={40}
+            fill="#89b717"
+            opacity={0.8}
+            draggable
+            rotation={star.rotation}
+            shadowColor="black"
+            shadowBlur={10}
+            shadowOpacity={0.6}
+            shadowOffsetX={star.isDragging ? 10 : 5}
+            shadowOffsetY={star.isDragging ? 10 : 5}
+            scaleX={star.isDragging ? 1.2 : 1}
+            scaleY={star.isDragging ? 1.2 : 1}
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
+          />
+        ))}
       </Layer>
     </Stage >
   );
